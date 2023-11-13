6f1d2852c208fb8c7f2439a5173ff0a69bc05f0f
Alex <admin@smartcalc.org>
2023-11-13 13:44:39 +0100

    Game status in game list. TODO added.

--- Scrabble3/Core/Game/Views/GameListView.swift
+++ Scrabble3/Core/Game/Views/GameListView.swift
@@ -22,8 +22,16 @@ struct GameListView: View {
                                 .navigationBarBackButtonHidden()
                                 .toolbar(.hidden, for: .tabBar)
                         } label: {
-                            Text(item.creatorUser.name ?? "")
-                            Text ("\(Utils.formatTransactionTimestamp(item.createdAt))")
+                            HStack {
+                                Text(item.creatorUser.name ?? "")
+                                Text ("\(Utils.formatTransactionTimestamp(item.createdAt))")
+                                Spacer()
+                                HStack(spacing: 12) {
+                                    Text(gameStatusLabel(game: item))
+                                    Image(systemName: gameStatusIcon(game: item))
+                                        .frame(width: 24)
+                                }
+                            }
                         }
                     }
                 }
@@ -61,8 +69,16 @@ struct GameListView: View {
                                 .navigationBarBackButtonHidden()
                                 .toolbar(.hidden, for: .tabBar)
                         } label: {
-                            Text(item.creatorUser.name ?? "")
-                            Text ("\(Utils.formatTransactionTimestamp(item.createdAt))")
+                            HStack {
+                                Text(item.creatorUser.name ?? "")
+                                Text ("\(Utils.formatTransactionTimestamp(item.createdAt))")
+                                Spacer()
+                                HStack(spacing: 12) {
+                                    Text(gameStatusLabel(game: item))
+                                    Image(systemName: gameStatusIcon(game: item))
+                                        .frame(width: 24)
+                                }
+                            }
                         }
                     }
                 }
@@ -93,6 +109,41 @@ struct GameListView: View {
             viewModel.removeListenerForArchivedGames()
             print("Games root view disappeared - remove game list listeners")
         }
+    }
+    
+    // TODO: we have to show different status icon and label for waiting games.
+    // - game creator waiting for other users.
+    // - game available for joining by another player.
+    // - game with two or more players - ready to start.
+    
+    func gameStatusLabel(game: GameModel) -> String {
+        if game.gameStatus == .waiting {
+            return "Ожидание игроков"
+        } else if game.gameStatus == .running {
+            return "Идет игра"
+        } else if game.gameStatus == .suspended {
+            return "Приостановлена"
+        } else if game.gameStatus == .finished {
+            return "Окончена"
+        } else if game.gameStatus == .aborted {
+            return "Отменена"
+        }
+        return ""
+    }
+    
+    func gameStatusIcon(game: GameModel) -> String {
+        if game.gameStatus == .waiting {
+            return "person.2.badge.gearshape"
+        } else if game.gameStatus == .running {
+            return "play.fill"
+        } else if game.gameStatus == .suspended {
+            return "playpause.fill"
+        } else if game.gameStatus == .finished {
+            return "flag.2.crossed.fill"
+        } else if game.gameStatus == .aborted {
+            return "xmark.circle.fill"
+        }
+        return ""
     }
     
 }
